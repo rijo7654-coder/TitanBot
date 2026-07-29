43f5ba56d55c9091fdb12cd8f5eb06e9fce01f6a
rijo7654-coder <rijo7654@gmail.com>
2026-07-29 18:45:22 +0530

    Update addupi.js

--- src/commands/Utility/src/commands/Utility/addupi.js
+++ src/commands/Utility/src/commands/Utility/addupi.js
@@ -1,1 +1,27 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 
+let upiId = "";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("addupi")
+    .setDescription("Set the UPI ID")
+    .addStringOption(option =>
+      option
+        .setName("upi")
+        .setDescription("Enter the UPI ID")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    upiId = interaction.options.getString("upi");
+
+    await interaction.reply({
+      content: `✅ UPI has been set to:\n\`${upiId}\``,
+      ephemeral: true,
+    });
+  },
+};
+
+export { upiId };
